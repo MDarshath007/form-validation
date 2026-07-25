3f90d22ccf1585b2274b970e17d3a0ea7834cf59
MDarshath007 <mohammedarshath1712@gmail.com>
2026-07-25 20:09:14 +0530

    sign up button modified

--- src/components/SignupSection.jsx
+++ src/components/SignupSection.jsx
@@ -169,7 +169,7 @@ function SignupSection() {
         <h6 className="text-center">OR</h6>
        
        <GoogleLogin 
-          width="472"         
+          width="100%"         
           onSuccess={(credentialResponse)=>{
           console.log(credentialResponse)
           navigate('/account-created')
